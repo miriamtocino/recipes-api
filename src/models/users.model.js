e83b04fdbe6b297d5186c20f340ee033cd10405e
Miriam Tocino <miriam.tocino@gmail.com>
2017-10-05 11:43:28 +0200

    Update Users Schema

--- src/models/users.model.js
+++ src/models/users.model.js
@@ -5,11 +5,12 @@
 module.exports = function (app) {
   const mongooseClient = app.get('mongooseClient');
   const users = new mongooseClient.Schema({
-  
+
+    name: {type: String, required: true},
     email: {type: String, unique: true},
     password: { type: String },
-  
-  
+
+
     createdAt: { type: Date, default: Date.now },
     updatedAt: { type: Date, default: Date.now }
   });
