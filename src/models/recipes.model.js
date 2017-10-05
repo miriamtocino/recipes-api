6826a7ffc20df00d5c25caf75ffa93469faf9cc9
Miriam Tocino <miriam.tocino@gmail.com>
2017-10-05 11:40:34 +0200

    Create Recipe Schema

--- src/models/recipes.model.js
+++ src/models/recipes.model.js
@@ -5,8 +5,31 @@
 module.exports = function (app) {
   const mongooseClient = app.get('mongooseClient');
   const { Schema } = mongooseClient;
+
+  const ingredientSchema = new Schema({
+    amount: { type: String, required: false },
+    name: { type: String, required: true },
+    optional: { type: Boolean, required: true, 'default': false }
+  });
+
+  const cookingStepSchema = new Schema({
+    cookingTime: { type: Number, required: false }, // in minutes
+    title: { type: String, required: false },
+    description: { type: String, required: true }
+  });
+
   const recipes = new Schema({
-    text: { type: String, required: true },
+    title: { type: String, required: true },
+    summary: { type: String, required: true },
+    photo: { type: String, default: 'http://via.placeholder.com/500x180?text=No%20Image' },
+    vegan: { type: Boolean, default: false },
+    vegetarian: { type: Boolean, default: false },
+    pescatarian: { type: Boolean, default: false },
+    cookingTime: { type: Number, required: false }, // in minutes
+    ingredients: [ingredientSchema],
+    cookingSteps: [cookingStepSchema],
+    likedBy: [{ type: Schema.Types.ObjectId, ref: 'users' }],
+    authorId: { type: Schema.Types.ObjectId, ref: 'users' },
     createdAt: { type: Date, default: Date.now },
     updatedAt: { type: Date, default: Date.now }
   });
