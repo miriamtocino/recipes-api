02eb5c2d16da4bf842c75c0504394054e28d2a0d
Miriam Tocino <miriam.tocino@gmail.com>
2017-10-05 11:41:00 +0200

    Generate Authentication Service

--- src/services/index.js
+++ src/services/index.js
@@ -1,5 +1,7 @@
 const recipes = require('./recipes/recipes.service.js');
+const users = require('./users/users.service.js');
 module.exports = function () {
   const app = this; // eslint-disable-line no-unused-vars
   app.configure(recipes);
+  app.configure(users);
 };
