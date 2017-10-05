946ab94765e15872fe42994c72bf3a1022b037f6
Miriam Tocino <miriam.tocino@gmail.com>
2017-10-05 12:11:47 +0200

    Add authentication client to seeds file

--- src/seeds.js
+++ src/seeds.js
@@ -1,3 +1,10 @@
+/* eslint-disable no-console */
+const feathers = require('feathers/client');
+const rest = require('feathers-rest/client');
+const superagent = require('superagent');
+const hooks = require('feathers-hooks');
+const auth = require('feathers-authentication-client');
+
 const user = {
   name: 'Jamie Gulliver',
   email: 'jamie@gulliver.dev',
@@ -270,3 +277,36 @@ const recipes = [
     cookingTime: 45,
   }
 ];
+
+// Seed the user and recipe!
+const feathersClient = feathers();
+
+feathersClient
+  .configure(hooks())
+  .configure(rest('http://localhost:3030').superagent(superagent))
+  .configure(auth());
+
+feathersClient.service('users').create(user)
+  .then(() => {
+    feathersClient.authenticate({
+      strategy: 'local',
+      email: user.email,
+      password: user.password
+    })
+      .then(() => {
+        recipes.map((recipe) => {
+          feathersClient.service('recipes').create(recipe)
+            .then((result) => {
+              console.log('Recipe seeded...', result.title);
+            }).catch((error) => {
+              console.error('Error seeding recipe!', error.message);
+            });
+        });
+      })
+      .catch(function(error){
+        console.error('Error authenticating!', error);
+      });
+  })
+  .catch(function(error) {
+    console.error('Error creating user!', error);
+  });
